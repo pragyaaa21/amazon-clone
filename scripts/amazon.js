f0edea0f45b447b8a3e35597b699ae06d96e6aee
pragyaaa <cppragya59@gmail.comm>
2025-07-02 19:04:47 +0530

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -72,7 +72,11 @@ matchingItem=item;
     });
     
     }
+    let cartQuantity=0;
+   cart.forEach((item)=>{
+    cartQuantity+=item.quantity;
+   });
+   document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
    
-    console.log(cart);
   });
 });
