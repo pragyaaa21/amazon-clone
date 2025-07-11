ffe9cbc1d5cedbf96d49b927bfd5b39897e9ca5b
pragyaaa <cppragya59@gmail.comm>
2025-07-11 19:38:31 +0530

    delete from html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -11,8 +11,10 @@ cart.forEach((cartItem)=>{
               matchingProduct=product;
             }
             });
+
+
             cartSummaryHTML+=`
-            <div class="cart-item-container">
+            <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -96,5 +98,9 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
    const productId=link.dataset.productId;
    console.log(productId);
    removeFromCart(productId);
+   const container = document.querySelector(`.js-cart-item-container-${productId}`
+
+   );
+   container.remove();
   })
 })
