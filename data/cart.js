2393ef8110d6e8797d5be3b0aac61ce46387c136
pragyaaa <cppragya59@gmail.comm>
2025-07-11 19:26:56 +0530

    delete from cart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart=[{
+export let cart=[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2,
 },{
@@ -21,5 +21,14 @@ export function addToCart(productId){
     });
     
     }
+
+}export function removeFromCart(productId){
+  const newCart=[];
+  cart.forEach((cartitem)=>{
+    if(cartitem.productId!=productId){
+      newCart.push(cartitem);
+    }
+  })
+cart=newCart;
 
 }
